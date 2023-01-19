b27ca41f280be33a4114d9ef9316850a98007aa1
Kaden Bautista <k07bautista@gmail.com>
2023-01-18 21:38:54 -0800

    made new libgdx project

---
+++ android/src/com/yuuna/anotherworldtd/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.yuuna.anotherworldtd;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.yuuna.anotherworldtd.Isekai;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Isekai(), config);
+	}
+}
---
+++ desktop/src/com/yuuna/anotherworldtd/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.yuuna.anotherworldtd;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.yuuna.anotherworldtd.Isekai;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Defending My Friends In Another World");
+		new Lwjgl3Application(new Isekai(), config);
+	}
+}
